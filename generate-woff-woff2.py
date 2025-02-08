f70f8475c55c6ec0825269da4d41cc3b75656bdf
Cristiano Sobral <cssobral2013@gmail.com>
2025-02-08 20:41:57 -0300

    Update v1.0

---
+++ generate-woff-woff2.py
@@ -0,0 +1,307 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+
+srcPath  = './ttf/Jenssen-ExtraLight.ttf'
+woffPath = './web/Jenssen-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/Jenssen-ExtraLightItalic.ttf'
+woffPath = './web/Jenssen-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/Jenssen-Light.ttf'
+woffPath = './web/Jenssen-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-LightItalic.ttf'
+woffPath = './web/Jenssen-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-LightItalic.ttf'
+woffPath = './web/Jenssen-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-Regular.ttf'
+woffPath = './web/Jenssen-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-Italic.ttf'
+woffPath = './web/Jenssen-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-Medium.ttf'
+woffPath = './web/Jenssen-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-MediumItalic.ttf'
+woffPath = './web/Jenssen-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBold.ttf'
+woffPath = './web/Jenssen-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBoldItalic.ttf'
+woffPath = './web/Jenssen-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBold.ttf'
+woffPath = './web/Jenssen-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBoldItalic.ttf'
+woffPath = './web/Jenssen-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-Bold.ttf'
+woffPath = './web/Jenssen-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-BoldItalic.ttf'
+woffPath = './web/Jenssen-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-ExtraBold.ttf'
+woffPath = './web/Jenssen-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-ExtraBoldItalic.ttf'
+woffPath = './web/Jenssen-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-Black.ttf'
+woffPath = './web/Jenssen-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/Jenssen-BlackItalic.ttf'
+woffPath = './web/Jenssen-BlackItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+
+srcPath  = './ttf/Jenssen-ExtraLight.ttf'
+woff2Path = './web/Jenssen-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/Jenssen-ExtraLightItalic.ttf'
+woff2Path = './web/Jenssen-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/Jenssen-Light.ttf'
+woff2Path = './web/Jenssen-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-LightItalic.ttf'
+woff2Path = './web/Jenssen-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-LightItalic.ttf'
+woff2Path = './web/Jenssen-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-Regular.ttf'
+woff2Path = './web/Jenssen-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-Italic.ttf'
+woff2Path = './web/Jenssen-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-Medium.ttf'
+woff2Path = './web/Jenssen-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-MediumItalic.ttf'
+woff2Path = './web/Jenssen-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBold.ttf'
+woff2Path = './web/Jenssen-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBoldItalic.ttf'
+woff2Path = './web/Jenssen-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBold.ttf'
+woff2Path = './web/Jenssen-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-SemiBoldItalic.ttf'
+woff2Path = './web/Jenssen-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-Bold.ttf'
+woff2Path = './web/Jenssen-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-BoldItalic.ttf'
+woff2Path = './web/Jenssen-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-ExtraBold.ttf'
+woff2Path = './web/Jenssen-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-ExtraBoldItalic.ttf'
+woff2Path = './web/Jenssen-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-Black.ttf'
+woff2Path = './web/Jenssen-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/Jenssen-BlackItalic.ttf'
+woff2Path = './web/Jenssen-BlackItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
